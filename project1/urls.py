e1c4015013f0fdf201c84abcf1d0825577c0815f
tvinh321 <tvinh321@gmail.com>
2021-11-17 13:32:46 +0700

    New Code

--- project1/urls.py
+++ project1/urls.py
@@ -18,22 +18,14 @@ from django.contrib import admin
 from django.urls import path
 from django.urls.conf import re_path
 from movie import views
+from movie.models import HeThongRap
 from . import settings
 from project1.settings import STATIC_URL
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('api/QuanLyPhim/XoaPhim/<int:pk>', views.QuanLyPhimDelete.as_view()),
     path('api/QuanLyPhim/LayDanhSachPhim', views.QuanLyPhimList.as_view()),
-    path('api/QuanLyPhim/ThemPhim', views.QuanLyPhimCreate.as_view()),
     path('api/QuanLyRap/LayThongTinHeThongRap',views.LayThongTinHeThongRapList.as_view()), #Xem He Thong Rap
-    path('api/QuanLyRap/ThemThongTinHeThongRap',views.LayThongTinHeThongRapCreate.as_view()), #Them He Thong Rap
-    path('api/QuanLyRap/XoaThongTinHeThongRap/<pk>',views.LayThongTinHeThongRapDelete.as_view()),  #Xoa He Thong Rap
     path('api/QuanLyRap/LayThongTinCumRapTheoHeThong',views.LayThongTinCumRapList.as_view()),#Xem Cụm Rạp
-    path('api/QuanLyRap/ThemThongTinCumRapTheoHeThong',views.LayThongTinCumRapCreate.as_view()), #Thêm Cụm Rạp
-    path('api/QuanLyRap/XoaThongTinCumRapTheoHeThong/<pk>',views.LayThongTinCumRapDelete.as_view()), #Xoá cụm rap
-    path('api/QuanLyRap/LayThongTinCumRapTheoHeThong/LayThongTinRap',views.RapList.as_view()), #Xem từng rạp TRONG cụm rạp
-    path('api/QuanLyRap/LayThongTinCumRapTheoHeThong/ThemThongTinRap',views.RapCreate.as_view()),  # Thêm rạp
-    path('api/QuanLyRap/LayThongTinCumRapTheoHeThong/XoaThongTinRap/<int:pk>',views.RapDelete.as_view()), # Xoá rạp
     path('api/QuanLyRap/LayThongTinLichChieuPhim',views.LayThongTinLichChieuPhimList.as_view()), #Xem lịch chiếu
 ]
